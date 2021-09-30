433d28d18a5ae6d6fa0d4dff985d90946098d888
hectormg24 <64148157+hectormg24@users.noreply.github.com>
2021-09-29 20:59:15 -0400

    Boton de actualizar y eliminar por modal, colocado
    No elimina el seleccionado, elimina el ultimo agregado.

--- proyectolab/resources/views/pensum/registrationpensum.blade.php
+++ proyectolab/resources/views/pensum/registrationpensum.blade.php
@@ -17,7 +17,7 @@
 
       <a href="pensums/create" class="btn btn-primary" style="margin-top: 20px;">CREAR</a>
 
-      <!-- Button trigger modal -->
+      <!-- Button Crear Pensum -->
       <button type="button" class="btn btn-primary" style="margin-top: 20px;" data-bs-toggle="modal" data-bs-target="#ModalCrearPensum">
         Crear Pensum
       </button>
@@ -36,29 +36,38 @@
 
         <tbody>    
           @foreach ($pensums as $pensum)
-          <tr>
-                  
-            <td>{{$pensum->id}}</td>
-            <td>{{$pensum->id_program}}</td>
-            <td>{{$pensum->descrip_pensum}}</td>
-            <td>{{$pensum->date}}</td>
-              
-            <td>
-              <form action="{{ route('pensums.destroy',$pensum->id) }}" method="POST">
-                <a href="/pensums/{{$pensum->id}}/edit" class="btn btn-info">Editar</a>         
-                  @csrf
-                  @method('DELETE')
-                <button type="submit" class="btn btn-danger">Delete</button>
+            <tr>
+                    
+              <td>{{$pensum->id}}</td>
+              <td>{{$pensum->id_program}}</td>
+              <td>{{$pensum->descrip_pensum}}</td>
+              <td>{{$pensum->date}}</td>
                 
-              </form>          
-            </td>        
-          </tr>
+              <td>
+                <form action="{{ route('pensums.destroy',$pensum->id) }}" method="POST">
+                  <a href="/pensums/{{$pensum->id}}/edit" class="btn btn-info" >Editar</a>         
+                    @csrf
+                    @method('DELETE')
+                  <button type="submit" class="btn btn-danger">Delete</button>
+                  
+                </form>          
+              </td>        
+            </tr>
           @endforeach
 
         </tbody>
       </table>
+      /pensums/{{$pensum->id}}/edit
+      
+      <form action="{{ route('pensums.destroy',$pensum->id) }}" method="POST">
+        <a href="#" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#ModalEditarPensum">Editar</a>         
+        @csrf
+        @method('DELETE')
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#ModalEliminarPensum">Delete</button>
+                
+      </form>  
 
-      <!-- Modal -->
+      <!-- Modal Crear Pensum -->
       <div class="modal fade" id="ModalCrearPensum" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
@@ -88,7 +97,7 @@
                   <input id="status" name="status" type="hidden" value="A">
                 </div>
                 
-                <a class="btn btn-secondary text-white" data-bs-dismiss="modal" tabindex="5">Cancelar</a>
+                <a href="#" class="btn btn-secondary" data-bs-dismiss="modal" tabindex="5">Cancelar</a>
                 <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
               </form>
             
@@ -97,7 +106,79 @@
         </div>
       </div>
 
+      <!-- Modal Editar Pensum -->
+      <div class="modal fade" id="ModalEditarPensum" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Editar Pensum</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form action="{{$pensum->id}}" method="POST">
+                @csrf    
+                @method('PUT')
+                <div class="mb-3">
+                  <label for="" class="form-label">Código del Programa:</label>
+                  <input id="id_program" name="id_program" type="number" class="form-control"  value="{{$pensum->id_program}}">    
+                </div>
+                  
+                  
 
+                <div class="mb-3">
+                  <label for="" class="form-label">Descripción:</label>
+                  <input id="descrip_pensum" name="descrip_pensum" type="text" class="form-control" value="{{$pensum->descrip_pensum}}">
+                </div>
+
+
+                <div class="mb-3">
+                  <label for="" class="form-label">Fecha:</label>
+                  <input id="date" name="date" type="date" class="form-control" value="{{$pensum->date}}">
+                </div>
+                  
+
+                <div class="mb-3">
+                  <input id="status" name="status" type="hidden" value="{{$pensum->status}}">
+                </div>
+
+                <a href="#" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</a>
+                <button type="submit" class="btn btn-primary">Guardar</button>
+              </form>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <!-- Modal Eliminar Pensum -->
+       <div class="modal fade" id="ModalEliminarPensum" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Confirmación</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body justify-content-center">
+
+              <h4 class="just-align-center text-dark">¿Seguro que desea eliminar este pensum?</h4>
+              
+            
+            </div>
+
+            <div class="modal-footer justify-content-end">
+              <form action="{{ route('pensums.destroy',$pensum->id) }}" method="POST">
+                <a href="#" class="btn btn-danger" data-bs-dismiss="modal">No</a>         
+                  @csrf
+                  @method('DELETE')
+                  <button type="submit" class="btn btn-success ">Si</button>
+
+                </form>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
 
     </div>
 
